9804837f380521bbcd97643da5f94748770b572f
PedroSaldivar <pedroantonio15901@outlook.es>
2025-06-04 03:45:35 -0600

    Final

--- Assets/Scripts/PanelSwitcher.cs
+++ Assets/Scripts/PanelSwitcher.cs
@@ -6,17 +6,17 @@ public class PanelSwitcher : MonoBehaviour
     public GameObject panelGame1;
     public GameObject panelGame2;
 <<<<<<< HEAD
+<<<<<<< HEAD
     public GameObject minimap;
 =======
     public KeyTracker keyTracker;
 >>>>>>> b1244a1516988fff1e1d8eebee47d0f09eb62e59
+=======
+>>>>>>> parent of 096e887 (juego 2 terminado)
     public GameManager gameManager;
-    public GameObject game2Location;
 
     public void OpenPanelGame1()
     {
-        minimap.SetActive(true);
-        game2Location.SetActive(false);
         panelDashboard.SetActive(false);
         panelGame1.SetActive(true);
 
@@ -36,16 +36,12 @@ public class PanelSwitcher : MonoBehaviour
 
     public void OpenPanelGame2()
     {
-        minimap.SetActive(false);
-        game2Location.SetActive(false);
         panelDashboard.SetActive(false);
         panelGame2.SetActive(true);
     }
 
     public void ReturnToDashboard()
     {
-        game2Location.SetActive(true);
-        minimap.SetActive(true);
         panelGame1.SetActive(false);
         panelGame2.SetActive(false);
         panelDashboard.SetActive(true);
